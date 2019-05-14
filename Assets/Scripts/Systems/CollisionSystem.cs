05dbc63ce7c360a4dc5a6de78a60963c5e085391
Ronald Goennier <40927071+ronman234@users.noreply.github.com>
2019-05-14 14:15:14 -0700

    Basic Enemy Movement
    Created enemy movement that casts a ray to see if it near a wall and it turns around and loops through the motions

---
+++ Assets/Scripts/Systems/CollisionSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class CollisionSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+
+        Entities.ForEach((EnemyMovementComponent enemyMovement, BoxCollider collider) =>
+        {
+            
+        });
+    }
+}
---
+++ Assets/Scripts/Systems/EnemyMovementSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class EnemyMovementSysytem : ComponentSystem
+{   
+    protected override void OnUpdate()
+    {
+        float time = Time.deltaTime;
+
+        Entities.ForEach((EnemyMovementComponent enemyMovement, Transform translation) =>
+        {
+            if(enemyMovement.isMovingRight)
+            {
+                translation.position += enemyMovement.direction * enemyMovement.moveSpeed * time;
+            }
+            else
+            {
+                translation.position += enemyMovement.direction * enemyMovement.moveSpeed * time;
+            }
+        });
+    }
+}
