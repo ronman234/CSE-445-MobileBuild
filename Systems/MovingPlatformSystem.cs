09ed322c0b19c9fd037c39609d0b201fa9e570dc
darkoprk <45085521+darkoprk@users.noreply.github.com>
2019-05-15 19:43:57 -0700

    player functionality with platform behaviors
    Has player functions for movement and jumping up and down. Also a set up for platform falling down (not fully functional having on collision issues with player and delay time of fall)

---
+++ Systems/MovingPlatformSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class MovingPlatformSystem : ComponentSystem
+{
+    public MovingPlatformComponent platform;
+
+    protected override void OnUpdate()
+    {
+        var time = Time.deltaTime;
+
+        Entities.ForEach((MovingPlatformComponent movingplatform) => 
+        {
+            movingplatform.currentPoint = movingplatform.points[movingplatform.pointSelection];
+
+            movingplatform.platform.transform.position = Vector3.MoveTowards (movingplatform.platform.transform.position, movingplatform.currentPoint.position, time * movingplatform.moveSpeed);
+
+            if (movingplatform.platform.transform.position == movingplatform.currentPoint.position)
+            {
+                movingplatform.pointSelection++;
+
+                if (movingplatform.pointSelection == movingplatform.points.Length)
+                {
+                    movingplatform.pointSelection = 0;
+                }
+                movingplatform.currentPoint = movingplatform.points [movingplatform.pointSelection];
+            }
+        });
+    }
+
+}
---
+++ Systems/PlayerMovementSystem.cs
@@ -0,0 +1,64 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PlayerMovementSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+
+        Entities.ForEach((TagPlayerComponent tagPlayer, Rigidbody2D rigidbody2D, InputComponent inputcomponent, MoveSpeedComponent movespeedcomponent) =>
+        {
+
+            if (inputcomponent.Movement > 0) //allows player movement with right arrow or D
+            {
+                inputcomponent.RightTouchKey = true;
+                rigidbody2D.velocity = new Vector2(inputcomponent.Movement * movespeedcomponent.moveSpeed, rigidbody2D.velocity.y);
+                movespeedcomponent.transform.eulerAngles = new Vector2(0, 0); //faces the player object in the right direction
+            }
+            else
+            {
+                inputcomponent.RightTouchKey = false;
+            }
+
+            if (inputcomponent.Movement < 0) //allows player movement with left arrow or A
+            {
+                inputcomponent.LeftTouchKey = true;
+                rigidbody2D.velocity = new Vector2(inputcomponent.Movement * movespeedcomponent.moveSpeed, rigidbody2D.velocity.y);
+                movespeedcomponent.transform.eulerAngles = new Vector2(0, 180);  //faces the player object in the left direction
+            }
+            else
+            {
+                inputcomponent.LeftTouchKey = false;
+            }
+
+            //Does the ground check for the player
+            movespeedcomponent.isGrounded = Physics2D.OverlapCircle(movespeedcomponent.groundCheck.position, movespeedcomponent.groundCheckRadius, movespeedcomponent.Ground);
+
+
+            if (movespeedcomponent.isGrounded == true && inputcomponent.Jump) //allows the player to jump
+            {
+                inputcomponent.MiddleTouchKey = true;
+                movespeedcomponent.extraJumpsValue = movespeedcomponent.extraJumps;  //stores values of extra jumps
+                rigidbody2D.velocity = Vector2.up * movespeedcomponent.jumpSpeed;    //physics movement up the y axis
+            }
+            else
+            {
+                inputcomponent.MiddleTouchKey = false;
+            }
+
+            //allows the player extra jumps while in air
+            if (inputcomponent.Jump && movespeedcomponent.extraJumpsValue > 0 && movespeedcomponent.isGrounded == false)
+            {
+                inputcomponent.MiddleTouchKey = true;
+                rigidbody2D.velocity = Vector2.up * movespeedcomponent.jumpSpeed; //physics movement for extra jumps
+                movespeedcomponent.extraJumpsValue--;  //decreases each extra jump from player
+            }
+            else
+            {
+                inputcomponent.MiddleTouchKey = false;
+                inputcomponent.Jump = false; //returns to ground
+
+            }
+        });
+    }
+}
