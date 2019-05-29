f82642bb190c5588b7b69f08e4373a7e8ab37ccd
Juan Carlos <004244915@coyote.csusb.edu>
2019-05-28 17:01:36 -0700

    AudioManager
    Audio Manager system with some components

---
+++ Assets/Scripts/Components/Developers/AudioManagerComponent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManagerComponent : MonoBehaviour
+{
+    public AudioMixerGroup group1;
+    public AudioMixerGroup group2;
+    public AudioMixerGroup group3;
+    public SoundComponent[] sounds;
+
+    public AudioManagerSystem instance;
+
+}
---
+++ Assets/Scripts/Systems/Developers/AudioManagerSystem.cs
@@ -0,0 +1,67 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class AudioManagerSystem : ComponentSystem
+{
+    protected override void OnUpdate() 
+    {
+        Entities.ForEach((AudioManagerComponent audio, GameObject gobject, AudioSource clips) =>
+        {
+            if (audio.instance == null)
+            {
+                audio.instance = this;
+            }
+            else
+            {
+                Object.Destroy(gobject.gameObject);
+                Debug.Log("Destroyed previous manager");
+                return;
+            }
+            Object.DontDestroyOnLoad(gobject.gameObject);
+
+            foreach (SoundComponent s in audio.sounds)
+            {
+                s.source = clips;
+                s.source.clip = s.clip;
+
+                s.source.volume = s.volume;
+                s.source.pitch = s.pitch;
+                s.source.loop = s.loop;
+                s.source.outputAudioMixerGroup = s.mixer;
+            }
+
+            Play("Theme");
+            Play("LavaRising");
+            Play("MainMenu");
+            Play("FireBurning");
+
+            Mute("LavaRising");
+            Mute("Theme");
+            Mute("FireBurning");
+
+            void Play(string name)
+            {
+                SoundComponent s = System.Array.Find(audio.sounds, sound => sound.name == name);
+                if (s == null)
+                {
+                    Debug.LogWarning("Sound: " + name + " not found.");
+                    return;
+                }
+                s.source.Play();
+            }
+
+            void Mute(string name)
+            {
+                SoundComponent s = System.Array.Find(audio.sounds, sound => sound.name == name);
+                if (s == null)
+                {
+                    Debug.LogWarning("Sound " + name + " not found.");
+                    return;
+                }
+                s.source.volume = 0.0f;
+            }
+
+        });
+    }
+}
+
