4595dfcb2389405d478e89357206d349d556556d
Ronald Goennier <rgoennier@gmail.com>
2019-06-03 11:25:07 -0700

    Started Working On Enemy AI

---
+++ Assets/Scripts/Systems/Developers/GargoyleAttackSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class GargoyleAttackSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((GargoyleTagComponent tag, BoxCollider2D collider, Animator anim, Transform translation, EnemyMovementComponent movement, SpriteRenderer renderer) =>
+        {
+            RaycastHit2D hit = Physics2D.Raycast(translation.position , movement.direction, 1);
+            if(hit.collider.tag == "Player")
+            {
+                anim.SetBool("attack", true);
+                //renderer.flipY = true;
+            }
+        });
+    }
+}
---
+++ Assets/Scripts/Systems/Developers/GargoyleRaycastSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class GargoyleRaycastSystem : ComponentSystem
+{
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((EnemyMovementComponent enemyMovement, Transform translation, GargoyleTagComponent gTag, SpriteRenderer sprite) =>
+        {
+            RaycastHit2D hit = Physics2D.Raycast(new Vector3(translation.position.x + 0.1f, translation.position.y) + enemyMovement.direction, Vector3.down, 1);
+            Debug.DrawRay(new Vector3(translation.position.x + 0.1f, translation.position.y) + enemyMovement.direction, Vector3.down, Color.green);
+            if (hit.collider.tag == "Wall")
+            {
+                if (enemyMovement.isMovingRight)
+                {
+                    enemyMovement.direction = Vector3.left;
+                    enemyMovement.isMovingRight = false;
+                    sprite.flipX = true;
+                }
+                else
+                {
+                    enemyMovement.direction = Vector3.right;
+                    enemyMovement.isMovingRight = true;
+                    sprite.flipX = false;
+                }
+            }
+            
+        });
+    }
+}
