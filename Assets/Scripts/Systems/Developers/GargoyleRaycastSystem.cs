6434a7c60a38c242b28f6eafc1c9581980cbe368
Ronald Goennier <rgoennier@gmail.com>
2019-06-05 09:07:14 -0700

    Created Gargoyle Attack

--- Assets/Scripts/Systems/Developers/GargoyleRaycastSystem.cs
+++ Assets/Scripts/Systems/Developers/GargoyleRaycastSystem.cs
@@ -7,23 +7,24 @@ public class GargoyleRaycastSystem : ComponentSystem
 {
     protected override void OnUpdate()
     {
+        
         Entities.ForEach((EnemyMovementComponent enemyMovement, Transform translation, GargoyleTagComponent gTag, SpriteRenderer sprite) =>
         {
-            RaycastHit2D hit = Physics2D.Raycast(new Vector3(translation.position.x + 0.1f, translation.position.y) + enemyMovement.direction, Vector3.down, 1);
-            Debug.DrawRay(new Vector3(translation.position.x + 0.1f, translation.position.y) + enemyMovement.direction, Vector3.down, Color.green);
-            if (hit.collider.tag == "Wall")
+            RaycastHit2D hit = Physics2D.Raycast(new Vector3(translation.position.x + 0.001f, translation.position.y) + enemyMovement.direction, Vector3.down, 1);
+            //Debug.DrawRay(new Vector2(translation.position.x + 0.001f, translation.position.y), Vector3.down, Color.green);
+            if (!hit)
             {
                 if (enemyMovement.isMovingRight)
                 {
                     enemyMovement.direction = Vector3.left;
                     enemyMovement.isMovingRight = false;
-                    sprite.flipX = true;
+                    sprite.flipX = false;
                 }
                 else
                 {
                     enemyMovement.direction = Vector3.right;
                     enemyMovement.isMovingRight = true;
-                    sprite.flipX = false;
+                    sprite.flipX = true;
                 }
             }
             
