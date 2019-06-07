f078dcd11dcc91e2790669b6eb9a7c2b216c2950
Ronald Goennier <rgoennier@gmail.com>
2019-06-06 22:28:12 -0700

    Working on implementing more AI

--- Assets/Scripts/Systems/Developers/GargoyleAttackSystem.cs
+++ Assets/Scripts/Systems/Developers/GargoyleAttackSystem.cs
@@ -9,27 +9,10 @@ public class GargoyleAttackSystem : ComponentSystem
 
     protected override void OnUpdate()
     {
-        BoxCollider2D spearCollider = null;
-        Entities.ForEach((SpearComponent spear, BoxCollider2D collider) =>
-        {
-            spearCollider = collider;
-        });
 
         Entities.ForEach((GargoyleTagComponent tag, BoxCollider2D collider, Animator anim, Transform translation, EnemyMovementComponent movement, SpriteRenderer renderer) =>
         {
-            RaycastHit2D hit = Physics2D.Raycast(translation.position + movement.direction, movement.direction, 1);
-            //Debug.DrawRay(translation.position, movement.direction, Color.black);
-            if(hit.collider.tag == "Player")
-            {
-                anim.SetBool("Attack", true);
-                spearCollider.enabled = true;
-                //renderer.flipY = true;
-            }
-            else
-            {
-                anim.SetBool("Attack", false);
-                spearCollider.enabled = false;
-            }
+            
         });
     }
 }
--- Assets/Scripts/Systems/Developers/GargoyleRaycastSystem.cs
+++ Assets/Scripts/Systems/Developers/GargoyleRaycastSystem.cs
@@ -10,7 +10,7 @@ public class GargoyleRaycastSystem : ComponentSystem
         
         Entities.ForEach((EnemyMovementComponent enemyMovement, Transform translation, GargoyleTagComponent gTag, SpriteRenderer sprite) =>
         {
-            RaycastHit2D hit = Physics2D.Raycast(new Vector3(translation.position.x + 0.001f, translation.position.y) + enemyMovement.direction, Vector3.down, 1);
+            RaycastHit2D hit = Physics2D.Raycast(new Vector3(translation.position.x + 0.001f, translation.position.y) + enemyMovement.direction, Vector3.down, 2);
             //Debug.DrawRay(new Vector2(translation.position.x + 0.001f, translation.position.y), Vector3.down, Color.green);
             if (!hit)
             {
